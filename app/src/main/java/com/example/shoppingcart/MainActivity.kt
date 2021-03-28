c762c654ceda51a9aa8e73b58cdebed26f87eb89
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-03-28 17:47:12 +0800

    bottom navigation done

--- app/src/main/java/com/example/shoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcart/MainActivity.kt
@@ -2,13 +2,34 @@ package com.example.shoppingcart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val homeFragment = HomeFragment()
+        val menuFragment = MenuFragment()
+        val favoriteFragment = FavoriteFragment()
 
+        setFragment(homeFragment)
 
+        val bottomNav = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_home -> setFragment(homeFragment)
+                R.id.nav_menu -> setFragment(menuFragment)
+                R.id.nav_fav -> setFragment(favoriteFragment)
+            }
+            true
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/shoppingcart/MenuFragment.kt
@@ -0,0 +1,8 @@
+package com.example.shoppingcart
+
+import androidx.fragment.app.Fragment
+
+class MenuFragment : Fragment(R.layout.fragment_menu) {
+
+
+}
