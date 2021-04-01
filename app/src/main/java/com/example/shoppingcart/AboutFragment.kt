6516015e2cedf5bbfb33e07002f3677b631ff79c
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-04-02 00:17:10 +0800

    navbar menu, and added contact and about fragment

---
+++ app/src/main/java/com/example/shoppingcart/AboutFragment.kt
@@ -0,0 +1,16 @@
+package com.example.shoppingcart
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
