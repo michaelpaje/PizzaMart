4e11e52b58e8bf3b57350ef1372bb58735dfbd7a
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-04-01 18:25:23 +0800

    cart fragment added, added custom toolbar
    burger menu not working yet

--- app/src/main/java/com/example/shoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcart/MainActivity.kt
@@ -1,26 +1,35 @@
 package com.example.shoppingcart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val homeFragment = HomeFragment()
         val menuFragment = MenuFragment()
-        val favoriteFragment = FavoriteFragment()
+        val cartFragment = CartFragment()
+
+        val cartImg:ImageView = findViewById(R.id.cartImage)
+        cartImg.setOnClickListener {
+            setFragment(cartFragment)
+        }
 
         setFragment(homeFragment)
-
         val bottomNav = findViewById<com.google.android.material.bottomnavigation.BottomNavigationView>(R.id.bottom_navigation)
         bottomNav.setOnNavigationItemSelectedListener {
             when(it.itemId) {
                 R.id.nav_home -> setFragment(homeFragment)
                 R.id.nav_menu -> setFragment(menuFragment)
-                R.id.nav_fav -> setFragment(favoriteFragment)
             }
             true
         }
@@ -32,4 +41,6 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
     }
+
+
 }
