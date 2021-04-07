ccd27e0323480bdcf15bc9ae19cda707f26a889f
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2021-04-07 18:00:40 +0800

    contact and about

--- app/src/main/java/com/example/shoppingcart/ContactFragment.kt
+++ app/src/main/java/com/example/shoppingcart/ContactFragment.kt
@@ -10,6 +10,5 @@ class ContactFragment : Fragment(R.layout.fragment_contact) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
     }
 }
