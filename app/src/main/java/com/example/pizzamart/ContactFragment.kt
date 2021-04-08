098b819b341ee6a794b7fd19e1ae0c968d65866b
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2021-04-08 21:12:08 +0800

    contactfragment

--- app/src/main/java/com/example/pizzamart/ContactFragment.kt
+++ app/src/main/java/com/example/pizzamart/ContactFragment.kt
@@ -1,12 +1,29 @@
 package com.example.pizzamart
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ContactFragment : Fragment(R.layout.fragment_contact) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val contactname = activity?.findViewById<EditText>(R.id.input_name)?.text.toString()
+        val contactemail = activity?.findViewById<EditText>(R.id.input_email)?.text.toString()
+        val contactmessage = activity?.findViewById<EditText>(R.id.input_message)?.text.toString()
+        val contactSend = activity?.findViewById<Button>(R.id.contact_btn)
+        contactSend?.setOnClickListener {
+            if(contactname == "" || contactemail == "" || contactmessage == ""){
+                Toast.makeText(activity, "Please enter all fields!", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(activity, "Message sent successfully!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
