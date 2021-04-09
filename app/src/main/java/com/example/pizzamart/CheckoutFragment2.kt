c9fa47e9b6c657ccf6b712e25d4c36cfb327cd39
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-04-10 00:18:53 +0800

    w

--- app/src/main/java/com/example/pizzamart/CheckoutFragment2.kt
+++ app/src/main/java/com/example/pizzamart/CheckoutFragment2.kt
@@ -11,7 +11,7 @@ import androidx.fragment.app.FragmentTransaction
 class CheckoutFragment2 : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout, container, false)
+        return inflater.inflate(R.layout.fragment_checkout2, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
