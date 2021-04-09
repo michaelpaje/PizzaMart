b0963a2d0679e1efc758cd29ae0bc075aface607
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-04-10 01:10:53 +0800

    done? :3

--- app/src/main/java/com/example/pizzamart/CheckoutFragment2.kt
+++ app/src/main/java/com/example/pizzamart/CheckoutFragment2.kt
@@ -1,11 +1,14 @@
 package com.example.pizzamart
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 
 class CheckoutFragment2 : Fragment() {
@@ -23,6 +26,19 @@ class CheckoutFragment2 : Fragment() {
                 replace(R.id.fl_wrapper, f)
                 commit()
             }
+            val inflater: LayoutInflater = LayoutInflater.from(activity)
+            val v:View = inflater.inflate(R.layout.or_layout,null)
+            val dialog = AlertDialog.Builder(activity)
+            dialog.setView(v)
+            dialog.setIcon(R.drawable.pizzamart_logo)
+            dialog.setTitle("Did you receive your order?")
+            dialog.setPositiveButton("Yes", DialogInterface.OnClickListener { _, _ ->
+                Toast.makeText(activity, "Thank you, Order Again!", Toast.LENGTH_SHORT).show()
+            })
+            dialog.setNegativeButton("No", DialogInterface.OnClickListener { _, _ ->
+            })
+            dialog.show()
+
         },t)
     }
 }
