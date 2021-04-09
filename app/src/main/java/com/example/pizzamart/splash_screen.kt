0f5e88b0fb9a594223a0f181125fde53622c4116
camsmy <camille.monis@gmail.com>
2021-04-09 18:15:39 +0800

    splash screen version 1

--- app/src/main/java/com/example/pizzamart/splash_screen.kt
+++ app/src/main/java/com/example/pizzamart/splash_screen.kt
@@ -1,12 +1,20 @@
 package com.example.pizzamart
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class splash_screen: AppCompatActivity() {
+    private val SPLASH_TIME:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
         this.supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
+        },SPLASH_TIME)
     }
 }
