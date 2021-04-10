3c6914845093e19222cf9e3ea71e2e3fd0e93388
rinzuno <47714773+rinzuno@users.noreply.github.com>
2021-04-10 16:34:07 +0800

    push ko na lang

--- app/src/main/java/com/example/pizzamart/ContactFragment.kt
+++ app/src/main/java/com/example/pizzamart/ContactFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Toast
 
 class ContactFragment : Fragment(R.layout.fragment_contact) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val contactName = activity?.findViewById<EditText>(R.id.input_name)
-        val contactEmail = activity?.findViewById<EditText>(R.id.input_email)
-        val contactMessage = activity?.findViewById<EditText>(R.id.input_message)
+        val llC = activity?.findViewById<LinearLayout>(R.id.llContact)
+        val contactName = llC?.findViewById<EditText>(R.id.input_name)
+        val contactEmail = llC?.findViewById<EditText>(R.id.input_email)
+        val contactMessage = llC?.findViewById<EditText>(R.id.input_message)
         val gCN = contactName?.text.toString()
         val gCE = contactEmail?.text.toString()
         val gCM = contactMessage?.text.toString()
